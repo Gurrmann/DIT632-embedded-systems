f76508e0b4dfcc4f4822dff74dbac75b65b6f27b
Aleksandar Isakovski <aleisa@net.chalmers.se>
2021-01-26 20:04:04 +0100

    added comments

--- wp1/exerc_1_4.c
+++ wp1/exerc_1_4.c
@@ -3,32 +3,47 @@
 
 int main(int argc, char const *argv[])
 {
-    srand(time(NULL));
-    int randomN = rand() % 100 + 1;    
-    printf("%d", randomN , "\n");
-  
+    srand(time(NULL));              //sets the random number generator to use a seed of current time
+    int randomN = rand() % 100 + 1; //create a random integer
+
+    //printf("%d", randomN , "\n");
+
+    //infinite while loop
     while(1){
+
+        //variable declaration
         int number;
         int guessCount = 0;
-        
-        printf("enter a number in the range of 1-100: \n");
-        scanf("%d", &number);
-        
+
+        printf("enter a number in the range of 1-100: \n"); //print instructions to user
+        scanf("%d", &number);                               // get user input
+
+        // checks if the number entered is in the correct range
         if(number >= 1 && number <= 100){
+            //returns a message if the user guessed too low and increases guess counter by 1
             if(number < randomN){
                 printf("your guess is too low \n");
                 ++guessCount;
-            }else if (number > randomN){
+            }
+            //returns a message if the user guessed too high and increases guess counter by 1
+            else if (number > randomN)
+            {
                 printf("your guess is to high: \n");
                 ++guessCount;
-            }else{
+            }
+            //increase the guess count by one and tells the user that he has guessed successfully
+            else
+            {
                 ++guessCount;
                 printf("You have guessed %d times and your guess is correct!", guessCount);
                 break;
             }
         }
+        //prints a message that informs the user that the enetered number is out of range
         else
+        {
             printf("Numebr is out of range!! \n");
+        }
     }
 
     return 0;
