bcf6d6a10211b91a18c54f06846be5ee110c5eed
Gustav Skallberg <gskallberg.dev@gmail.com>
2021-01-29 14:21:26 +0100

    revamped logic for 1.1 and added comments

--- wp1/exerc_1_1.c
+++ wp1/exerc_1_1.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
 int main() {
 
-    bool exit = false;
+    
+   bool exit = false; // Program will run forever while this value is equal to false
 
+        
+        //Infinite loop until the user puts in a 0
         while (!exit) {
 
+            printf("Please enter a number between 1-5, press 0 to exit!\n");
             int userInput;
             scanf("%d", &userInput);
 
-        
-            switch(userInput) {
-
-                case 1:
-                printf("You Pressed 1\n");
-                break;
-
-                case 2:
-                printf("You Pressed 2\n");
-                break;
-
-                case 3:
-                printf("You pressed 3\n");
-                break;
-
-                case 4:
-                printf("You pressed 4\n");
-                break;
-
-                case 5:
-                printf("You pressed 5\n");
-                break;
-
-                case 0:
-                printf("Bye Bye!");
+            //prints out the number the user put in if it's in the range of 1-5
+             if (userInput > 0 && userInput < 6) printf("You Pressed %d\n", userInput);
+             
+             //exits the program
+             if (userInput == 0) {
+                printf("Bye Bye");
                 exit = true;
-            
-            }   
-
-        }       
-    
+             }
+             //prints if the user puts in a different number than asked for
+             else {
+                 printf("Please put in a valid number\n");
+             }
+    }
+    return 0;
 }
