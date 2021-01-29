b46370d04c4cb477cfcdb98478cc4a71d3513b09
Gustav Skallberg <gskallberg.dev@gmail.com>
2021-01-29 14:24:44 +0100

    Added comments

--- wp1/exerc_1_2.c
+++ wp1/exerc_1_2.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
-#define MAX 80
+#define MAX 80 // This value will decide how many characters that the user is allowed to input
 
 int main() {
 
     char userInput[MAX];
-    fgets(userInput, MAX, stdin);
+    fgets(userInput, MAX, stdin); // Gets the input from the user
     
     int words = 0;
 
+    //The loop counts how many spaces there are from the user and will then decide the amount of words that was put in
     for (size_t i = 0; i < MAX; i++)
     {
         if (userInput[i] == ' ') {
@@ -16,6 +17,7 @@ int main() {
         }
     }
     
+    //prints the amount of words
     printf("The number of words are %d!", words);
 
     return 0;
