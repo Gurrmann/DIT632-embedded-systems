4220511cb68e12c3b3d4cd9d0affc670da4d91a6
Gustav Skallberg <gskallberg.dev@gmail.com>
2021-01-30 15:48:15 +0100

    Now compares the strings

--- wp2/exerc_2_3.c
+++ wp2/exerc_2_3.c
@@ -10,13 +10,25 @@ int stringComparer(char *string1, char *string2);
 int main() {
 
     char test1[] = "hej";
-    char test2[] = "hejsan";
+    char test2[] = "hea";
 
     size_t arr1 = ARRAYSIZE(test1);
     size_t arr2 = ARRAYSIZE(test2);
     
 
-    if (arr1 == arr2) printf("not identical");
+    if (arr1 != arr2) printf("not identical");
+
+    else {
+        
+        for (size_t i = 0; i < arr1; i++)
+        {
+            if(test1[i] != (test2[i])) {
+                printf("not identical");
+                break;
+            }
+        }
+        
+    }
 
 
     return 0;
