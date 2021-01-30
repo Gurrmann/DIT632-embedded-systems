616b2c0d7c3652357bf87fbceac875af627c9af0
Gustav Skallberg <gskallberg.dev@gmail.com>
2021-01-30 16:18:21 +0100

    Minor logic change

--- wp2/exerc_2_3.c
+++ wp2/exerc_2_3.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+//macro to calculate the length of an array
 #define ARRAYSIZE(x) (sizeof(x) / sizeof((x)[0]))
-
-//initialising functions
-int stringSplicer();
-int stringComparer(char *string1, char *string2);
 
 int main() {
 
     char test1[] = "hej";
-    char test2[] = "hea";
+    char test2[] = "hej";
 
+    //Takes the length of the two arrays to see if they are the same
     size_t arr1 = ARRAYSIZE(test1);
     size_t arr2 = ARRAYSIZE(test2);
     
@@ -19,14 +17,20 @@ int main() {
     if (arr1 != arr2) printf("not identical");
 
     else {
-        
+        // if they are the same length it compares each element to see if there is a match
+        // if there isn't it will break the loop and alert the user
+
+        bool identical = true;
+
         for (size_t i = 0; i < arr1; i++)
         {
             if(test1[i] != (test2[i])) {
-                printf("not identical");
+                identical = false;
                 break;
             }
         }
+        // Prints out the result depending on if they are identical or not 
+        identical ? printf("The words are identical") : printf("the words are not identical");
         
     }
 
