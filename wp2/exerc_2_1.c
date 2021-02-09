17e8560120702edd605b550d5d1207afdb438d1b
Aleksandar Isakovski <aleisa@net.chalmers.se>
2021-02-09 20:14:47 +0100

    exerc1 fix?

--- wp2/exerc_2_1.c
+++ wp2/exerc_2_1.c
@@ -12,36 +12,25 @@ Demonstration code: 363840
 
 
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stddef.h> // so that we can assign NULL to pointer values
 
-#define MAX 20 //max number of chars in input
-#define SIZE 2048 //max str size read from file
+#define MAX_LENGHT 20
 
-void copyString(char *userInput2, char *copyStr2); //function prototype
+void copyString(char*, char*);
 
-int main() {
+int main(int argc, char const *argv[])
+{
+    char input[MAX_LENGHT];
+    char copiedString[MAX_LENGHT];
 
-    char stringInput[MAX]; //local string variable with user input
-    char anotherStr[MAX]; // copy string for a)
-    char anotherStr2[MAX]; //copy string for b)
+    fgets(input, MAX_LENGHT, stdin);
+    printf("Entered String is: %s \n", input);
 
-    printf("Please enter string to be copied:\n");
-    fgets (stringInput, MAX, stdin);  //reads f, stores content in read_str
+    copyString(input, copiedString);
+    printf("The coppied string is: %s \n", copiedString);
 
-    // a) Copying variable with lib function strcpy()
-    printf("\na) Copying variable with lib function strcpy().\n");
-    strcpy(anotherStr, stringInput); //copies userInput to anotherStr
-    printf("The value of the copy anotherStr is:\n%s", anotherStr);
-
-    // b) Your own function void copyString(...) not using any library function.
-    printf("\nb) Copying variable with our own function copyString() function.\n");
-    copyString(stringInput, anotherStr2);    //copies userInput into anotherStr2
-    printf("The value of the copy anotherStr2 is:\n%s\n", anotherStr2);
-
-    return 0; //returns 0 if everything is run properly
+    return 0;
 }
+
 
 void copyString(char *userInput, char *copy) {
     // char *strBeginning = copy; //starting address from string at copy
@@ -54,7 +43,7 @@ void copyString(char *userInput, char *copy) {
         copy++; //Move to the next char of the copy char array
     }
     //now copy is pointing to the index right after the last char of the array
-    *copy = '\0';   //Adds the end of string char '\0' 
-                    //to the end of the char array at the current index 
+    *copy = '\0';   //Adds the end of string char '\0'
+                    //to the end of the char array at the current index
                     //which *copy is pointing to
 }
