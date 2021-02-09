c0857e4c6e5b8f64b168341f95571df743590c55
hlebanja <thiagohtoliva@gmail.com>
2021-02-09 10:51:27 +0100

    add 2.7 skeleton

---
+++ wp2/exerc_2_7.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX 10
+
+//prototypes
+int readPersnr(char *person);
+int controlDigit(char *persnr);
+int breakDown(char *persnr, int start, int end);
+
+int main(int argc, char const *argv[])
+{
+    int persnr[MAX];
+    readPersnr(persnr);
+
+    printf("\n the persnr is \n");
+    printf("%s", persnr);
+
+
+    int year = breakDown(persnr, 0, 1);
+    int month = breakDown(persnr, 2, 3);
+    int day = breakDown(persnr, 4, 5);
+    int number = breakDown(persnr, 6, 8);
+    int lastDigit = breakDown(persnr, 9, 9);
+
+    return 0;
+}
+void readPersnr(char *person) { 
+    fgets(person, MAX, stdin);
+}
+
+//get a range from the char array persnr and return it as an int
+int breakDown(char *persnr, int start, int end) {
+    char tempArray[10]; //creates temp array
+    for (int i = start; i <= end; i++) { //iterates through range
+        tempArray[i] = persnr[i]; //extracts char
+    }
+    return atoi(tempArray); //concatenates tempArray and return it as an int
+}
