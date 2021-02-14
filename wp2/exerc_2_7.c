00b0674733283c99752d5d306b851dfd0e00a454
hlebanja <thiagohtoliva@gmail.com>
2021-02-14 17:09:58 +0100

    add folder

--- wp2/exerc_2_7.c
+++ wp2/exerc_2_7.c
@@ -15,7 +15,7 @@ Demonstration code: 363840
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 12
+#define MAX 10
 
 //prototypes
 void readPersnr(char *person);
@@ -24,23 +24,24 @@ int breakDown(char *persnr, int start, int end);
 
 int main()
 {
-    char persnr[MAX];
-    readPersnr(persnr);
+    // char persnr[MAX];
+    char persnr[] = "9006133590";
+    // readPersnr(persnr);
 
-    printf("the user personnumer is: %s \n", persnr);
+    // printf("the user personnumer is: %s \n", persnr);
     // 9006133590
 
-
     int year = breakDown(persnr, 0, 1);
-    printf("\nyear: %d", year);
-    // int month = breakDown(persnr, 2, 3);
-    // printf("\nmonth: %d", month);
-    // int day = breakDown(persnr, 4, 5);
-    // printf("\nday: %d", day);
-    // int number = breakDown(persnr, 6, 8);
-    // printf("\n3 digits: %d", number);
-    // int lastDigit = breakDown(persnr, 9, 9);
-    // printf("\nlastDigit: %d", lastDigit);
+    printf("\n\nyear: %d", year);
+    int month = breakDown(persnr, 2, 3);
+    printf("\nmonth: %d", month);
+    int day = breakDown(persnr, 4, 5);
+    printf("\nday: %d", day);
+    int number = breakDown(persnr, 6, 8);
+    printf("\n3 digits: %d", number);
+    int lastDigit = breakDown(persnr, 9, 9);
+    printf("\nlastDigit: %d\n\n", lastDigit);
+    printf("\nlastDigit: %d\n\n", lastDigit);
 
     
   // if (month > 12 || month < 1) {
@@ -59,25 +60,31 @@ void readPersnr(char *person) {
 
     for (int i = 0; i < MAX; i++) {
       *person = userInput[i];
-      *person++;
+      person++;
     }
 }
 
 //get a range from the char array persnr and return it as an int
 int breakDown(char *persnr, int start, int end) {
-    for (int i = 0; i < start; i++){ //sets pointer to desired start index
-      *persnr++;
-    }
-    
-    char tempArray[MAX]; //creates temp array
-    for (int i = 0; i <= start - end + 1; i++) { //iterates through range
-        tempArray[i] = *persnr; //extracts char
-        *persnr++;
-    }
-    printf("temp array is: %s\n", tempArray);
-    int result;
-    result = atoi(tempArray);
-    printf("result is: %d\n", result);
+  int size = end - start + 1;
+  char tempArray[size];
+  char *tempPtr = tempArray;
+  //set the pointer to the start index
+  for(int i = 0; i < start; i++) {
+    // printf("\n\nindex is: %d\n", i);
+    // printf("value is: %c\n\n", *persnr);
+    persnr++;
+  }
 
-    return result; //concatenates tempArray and return it as an int
+  //add from start to end the items to tempArray
+  while (start <= end)
+  {
+    *tempPtr = *persnr;
+    tempPtr++;
+    persnr++;
+    start++;
+  }
+
+  //atoi tempArray and return it.
+  return atoi(tempArray);
 }
