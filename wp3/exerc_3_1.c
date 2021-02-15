67f1d02594a1746c5acf38e053ff619a7dbf2d9e
hlebanja <thiagohtoliva@gmail.com>
2021-02-15 17:21:56 +0100

    add comments for ex 3.1

--- wp3/exerc_3_1.c
+++ wp3/exerc_3_1.c
@@ -14,12 +14,12 @@ Demonstration code:
 #include <stdio.h>
 #define MAX 20
 
-enum DIRECTION {N,E,S,W};
+enum DIRECTION {N,E,S,W}; //North, East, South, West
 
-typedef struct {
-  int xpos;
-  int ypos;
-  enum DIRECTION dir;
+typedef struct { //typedef so you don't have to type struc every time you create a new ROBOT
+  int xpos; //position on X axis
+  int ypos; //position on Y axis
+  enum DIRECTION dir; //direction N E S W equivalent to 0, 1, 2, 3
 } ROBOT;
 
 void initializeRobot(ROBOT*, int, int, enum DIRECTION);
@@ -29,8 +29,8 @@ void executeInstructions(ROBOT*, char[]);
 
 int main()
 {
-  ROBOT robot, *ptr;
-  ptr = &robot;
+  ROBOT robot, *ptr; //initializes Robot and pointer of type ROBOT.
+  ptr = &robot; //points ptr to robot address. 
   
   int x;
   int y;
@@ -38,22 +38,26 @@ int main()
 
   while(1) { //infinite loop
 
-  printf("\nReinitializing...\n\n");
+  printf("\n------------------------------------------------------------------\n");
+  printf("\nReinitializing...\n");
+  printf("\n------------------------------------------------------------------\n");
   printf("\nPlease inform starting position on the X AXIS\n");
-  scanf("%d", &x);
+  scanf("%d", &x); //get robot's starting position on the X axis
 
   printf("\nPlease inform starting position on the Y AXIS\n");
-  scanf("%d", &y);
-  initializeRobot(ptr, x, y, N);
+  scanf("%d", &y); //get robot's starting position on the Y axis
+  
+  initializeRobot(ptr, x, y, N); //initialized Robot with x, y and North direction.
 
   printf("\nPlease inform strings containing\nchar m for movements\nchar t for turning 90 degrees clockwise\n");
-  scanf("%s", userInput);
+  scanf("%s", userInput); //get string containing 'm' and 't'
 
   executeInstructions(ptr, userInput);
 
-  printf("the y is %d\n\n", robot.ypos);
-  printf("the x is %d\n\n", robot.xpos);
-  printf("the Robot direction is %d\n\n", ptr->dir);
+  printf("\n****Final Robot position****\n");
+  printf("->the y is %d\n", robot.ypos);
+  printf("->the x is %d\n", robot.xpos);
+  printf("->the Robot direction is %d\n\n", ptr->dir);
 
   }
   return 0;
@@ -61,39 +65,46 @@ int main()
 
 void executeInstructions(ROBOT *ptr, char instructions[]) {
   int i = 0;
-  while (instructions[i] != "\0") {
+  while (instructions[i] != '\0') { //while not the end of the string
     if (instructions[i] != 'm' && instructions[i] != 't') {
-      // printf("\n\ninvalid input detected\n\n"); //this is bugged, I need to fix it
+      printf("\n*********************************************************\n");
+      printf("\n------------>WARNING!! Invalid input detected<------------\n");
+      printf("\n*********************************************************\n");
       break;
     } else {
+      //if instruction is an m, call function move. Else, call function turn.
       instructions[i] == 'm' ? move(ptr) : turn(ptr);
     }
     i++;
   }
 }
 
+//set the robot with the informed x, y and direction.
 void initializeRobot(ROBOT *ptr, int x, int y, enum DIRECTION dir) {
-  ptr->xpos = x;
-  ptr->ypos = y;
-  ptr->dir = dir;
+  ptr->xpos = x; //set robot's xpos to x.
+  ptr->ypos = y; //same as (*ptr).ypos = y
+  ptr->dir = dir; //set robot's dir (always north here).
 }
 
+//move the robot by 1 unit in its current direction.
 void move (ROBOT *ptr) {
-  if (ptr->dir == 0) {//North
+  /*depending on which direction the robot is pointed to, 
+  increases or decreases the xpos or ypos */
+  if (ptr->dir == 0) {//North -> moves by 1 in the y axis
     ptr->ypos++;
   }
-  if (ptr->dir == 1) {//East
+  if (ptr->dir == 1) {//East -> moves by 1 in the x axis
     ptr->xpos++;
   }
-  if (ptr->dir == 2) {//South
+  if (ptr->dir == 2) {//South -> moves by -1 in the y axis
     ptr->ypos--;
   }
-  if (ptr->dir == 3) {//West
+  if (ptr->dir == 3) {//West -> moves by -1 in the x axis
     ptr->xpos--;
   }
 }
 
 void turn (ROBOT *ptr) {
-    ptr->dir++;
-    ptr->dir = ptr->dir % 4;
+    ptr->dir++; //increases dir by 1. {N, E, S, W} is equivalent to {0, 1, 2, 3}.
+    ptr->dir = ptr->dir % 4; //so that when you turn 4 times it goes back to 0 (NORTH)
 }
